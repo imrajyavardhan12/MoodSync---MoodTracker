65a60ab47391309105ce86e08638f73538fa5a8d
Rajyavardhan Singh <srajyavardhan12@gmail.com>
2025-03-20 16:04:22 +0530

    calender-feature added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const dateElement = document.getElementById('date');
 const timeElement = document.getElementById('time');
-
-
+const monthElement = document.getElementById('month');
+const weekElement = document.getElementById('week-days');
+const calender = document.getElementById('calendar');
+const backBtn = document.getElementById('back-btn');
+const nextBtn = document.getElementById('next-btn');
+let monthNo = 0;
 
 
 // to update date and time
@@ -25,9 +29,65 @@ timeElement.innerText = now.toLocaleTimeString('en-US',timeOptions);
 
 }
 
+
 setInterval(updateTime,1000);
 updateTime();
 
+// calender logic
+
+const weekDays = ['Sunday','Monday','Tuesday','Wednesday', 'Thursday','Friday','Saturday']; 
 
 
 
+
+function renderCalendar(){
+    const now = new Date();
+
+    if(monthNo != 0){
+        now.setMonth(new Date().getMonth()+ monthNo);
+    }
+    const day = now.getDate();
+    const month = now.getMonth()
+    const year = now.getFullYear();
+    // console.log(month)
+
+    
+    monthElement.innerText = `${now.toLocaleDateString('en-US',{month: "long"})} ${year}`
+    
+    const daysInMonth = new Date(year,month+1,0).getDate();
+    const firstDayOfMonth = new Date(year,month,1).toLocaleDateString('en-US',{
+        weekday : 'long'
+    })
+    
+    const paddingDays = weekDays.indexOf(firstDayOfMonth);
+
+    calender.innerText = ''
+    
+    for(let i = 1 ; i <= daysInMonth + paddingDays; i++){
+        const dayItem = document.createElement('div');
+        dayItem.classList.add('day');
+    
+        if(i <= paddingDays){
+            dayItem.classList.add('padding')
+        }
+        else{
+            dayItem.innerText = i - paddingDays;
+            if(((i - paddingDays) == day) && monthNo == 0 ){
+                dayItem.id = 'currentDay'
+            }
+        }
+        calender.appendChild(dayItem);
+    }  
+}
+
+renderCalendar();
+
+nextBtn.addEventListener('click',()=>{
+    monthNo++;
+    renderCalendar();
+})
+
+backBtn.addEventListener('click',()=>{
+    monthNo--;
+    renderCalendar();
+})
